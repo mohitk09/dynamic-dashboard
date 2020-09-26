983a3656f5b9d9673fef30906099f819e7e4e3ae
Mohit Khotani <mohit.khotani@gmail.com>
2020-09-26 18:12:40 +0530

    add dev dependencies and modify index file

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import 'react-dropdown/style.css';
+import './style.css';
+import "bootstrap/dist/css/bootstrap.css";
+import '@fortawesome/fontawesome-free/css/all.css';
 
 ReactDOM.render(
   <React.StrictMode>
