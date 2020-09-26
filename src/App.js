a06604bb8a9d42fbd249275b432799ec536c8729
Mohit Khotani <mohit.khotani@gmail.com>
2020-09-26 16:41:46 +0530

    console env variables

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  console.log('procss variav', process.env);
   return (
     <div className="App">
       <header className="App-header">
